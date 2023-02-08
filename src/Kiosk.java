4c6327ee2424583819b082443b06da0d40197b12
Dalton <dalton.m.towe@gmail.com>
2023-02-08 16:42:37 -0500

    Wrote code tested and added comments

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -2,7 +2,35 @@ public class Kiosk {
     public static void main(String[] args)
     {
 
-        
+        //class Overtwentyone
+        //	main()
+        //	    //Declare variables
+        //	    num age
+        //	    //Prompt and input
+        //	    output “Enter your age ”
+        //	    input age
+        //	    //Process
+        //	    if age >= 21 then
+        //          //Output
+        //	        output “You get a wrist band”
+        //	    endif
+        //	return
+        //end class
+
+        //Declare variables
+        int age;
+
+        //simulate input prompt
+        System.out.println("Enter your age ");
+
+        //Simulate input
+        age = 21;
+
+        if (age >= 21)
+        {
+            System.out.println("You get a wrist band");
+        }
+
 
     }
 }
